bc258e2416ce6905c9a7d665eb50be472f52207f
Glejdi <glenik19@htl-shkoder.com>
2025-04-22 12:48:59 +0200

    Created login

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { enhance } from '$app/forms';
+	import Warning from '$lib/components/Warning.svelte';
+	export let form;
+</script>
+
+<div>
+	<form action="?/login" method="POST" use:enhance>
+		<h1>Login</h1>
+
+		<label for="email">E-Mail</label>
+		<input type="text" name="email" id="email" placeholder="Enter your email" required />
+
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password" placeholder="Enter your password" required />
+
+		<button type="submit">Login</button>
+
+		{#if form}
+			<Warning message={form.message} />
+		{/if}
+	</form>
+</div>
