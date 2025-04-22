28a2edcca7d7cccb20330c107cc8ac6d41313c39
Glejdi <glenik19@htl-shkoder.com>
2025-04-22 14:10:42 +0200

    Added article to homepage

---
+++ src/routes/+page.server.js
@@ -0,0 +1,31 @@
+import { createConnection } from '$lib/db/mysql';
+
+export async function load({ locals }) {
+	let connection = await createConnection();
+	let [rows] = await connection.execute('SELECT * FROM articles LIMIT 25');
+	
+
+	return {
+		articles: rows,
+		user: locals.user
+	};
+}
+export const actions = {
+
+    like: async ({ request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id');
+
+		const connection = await createConnection();
+		const [result] = await connection.execute(
+			'UPDATE articles SET votes = votes + 1 WHERE id = ?',
+			[id]
+		);
+
+		if (result.affectedRows) {
+			return { success: true };
+		} else {
+			return { error: 'Failed' };
+		}
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { flip } from 'svelte/animate';
+
+	let { data } = $props();
+</script>
+
+<h1>Social Media</h1>
+<p>Explore Bilder by category or see them all at once.</p>
+
+<div>
+	{#each data.articles as article (article.id)}
+		<div animate:flip>
+			<a href={`articles/${article.id}`}>
+				<img src={article.image} alt="imageuploaded" />
+			</a>
+			<div>
+				<p>Author:</p>
+				<p>{article.author}</p>
+
+				<form action="?/like" method="POST" use:enhance>
+					<input type="hidden" name="id" value={article.id} />
+					<button type="submit" aria-label="like">
+						<img src="https://ue6hr90fzxvjnndo.public.blob.vercel-storage.com/SocialMedia/like-tOg0BsMbTdvrPRXL0DW2OWU4PVmmXp.png" alt="like" />
+					</button>
+				</form>
+				<div>
+					<span>Likes: {article.votes}</span>
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
