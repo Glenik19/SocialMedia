536323d5994dbf63b7a9b312bce9c8eff983af67
Glejdi <glenik19@htl-shkoder.com>
2025-04-22 12:46:43 +0200

    Created add a new article page

---
+++ src/routes/admin/new/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let { data } = $props();
+</script>
+
+<h1>Create a New Article</h1>
+
+<form method="POST" action="?/createArticle" enctype="multipart/form-data">
+    <div>
+		<label for="image">Image</label>
+		<input type="file" name="uploadedImage">
+	</div>
+
+	<div>
+		<label for="description">Description</label>
+		<textarea name="description" id="description" placeholder="Enter description"></textarea>
+	</div>
+
+	<div>
+		<label for="author">Author</label>
+		<input type="text" name="author" id="author" placeholder="Enter Author" />
+	</div>
+
+	<button type="submit">
+		Create Article
+	</button>
+</form>
